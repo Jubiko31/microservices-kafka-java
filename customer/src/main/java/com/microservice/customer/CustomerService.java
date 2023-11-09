5591e405e864e8e8e397f6e749108cb0dd944e0a
Jubiko31 <jubiko.macaberidze1@gmail.com>
2023-11-09 22:48:53 +0400

    implemented fraud controller and service

--- customer/src/main/java/com/microservice/customer/CustomerService.java
+++ customer/src/main/java/com/microservice/customer/CustomerService.java
@@ -1,6 +1,13 @@
 package com.microservice.customer;
 
-public record CustomerService(CustomerRepository customerRepository) {
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
---
+++ fraud/src/main/java/com/microservice/fraud/FraudCheckResponse.java
@@ -0,0 +1,5 @@
+package com.microservice.fraud;
+
+public record FraudCheckResponse(Boolean isFraud) {
+
+}
